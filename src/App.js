2e6a9d05e11e08b87d321315ff7c33d9b714a34d
Marta Johnsson <marta.johnsson@objektvision.se>
2023-09-27 21:58:27 +0200

    start templatestart template

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@
 const  App = ()=> {
   return (
     <div className="app">
-
+      <section className="side-bar">
+        <button>+ New chat</button>
+        <ul className="history"></ul>
+        <nav>
+          <p>Made by Marta</p>
+        </nav>
+      </section>
+      <section className="main"></section>
     </div>
   );
 }
