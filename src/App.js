ad4bd9d4554ab516c12aed44e3ebc9f69c62b89e
Marta Johnsson <marta.johnsson@objektvision.se>
2023-09-27 23:13:23 +0200

    styling and new html added, input and info

--- src/App.js
+++ src/App.js
@@ -4,12 +4,26 @@ const  App = ()=> {
     <div className="app">
       <section className="side-bar">
         <button>+ New chat</button>
-        <ul className="history"></ul>
+        <ul className="history">
+            <li>Test</li>
+        </ul>
         <nav>
           <p>Made by Marta</p>
         </nav>
       </section>
-      <section className="main"></section>
+      <section className="main">
+          <h1>MartaGTP</h1>
+          <ul className="feed"></ul>
+          <div className="bottom-section">
+            <div className="input-container">
+                <input/>
+                <div id="submit">➡</div>
+            </div>
+              <p className="info">
+                Chat GPT Mar 14 Version. Free Research Preview.
+              </p>
+          </div>
+      </section>
     </div>
   );
 }
